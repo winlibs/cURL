466426c1fad880f519f41ecd7bec14ddf20b38d4
Daniel Stenberg <daniel@haxx.se>
2020-08-16 11:34:35 +0200

    Curl_easy: remember last connection by id, not by pointer
    CVE-2020-8231
    
    Bug: https://curl.haxx.se/docs/CVE-2020-8231.html
    
    Reported-by: Marc Aldorasi
    Closes #5824
    
    (cherry picked from commit 3c9e021f86872baae412a427e807fbfa2f3e8a22)

--- lib/multi.c
+++ lib/multi.c
@@ -454,6 +454,7 @@ CURLMcode curl_multi_add_handle(struct Curl_multi *multi,
     data->state.conn_cache = &data->share->conn_cache;
   else
     data->state.conn_cache = &multi->conn_cache;
+  data->state.lastconnect_id = -1;
 
 #ifdef USE_LIBPSL
   /* Do the same for PSL. */
@@ -669,11 +670,11 @@ static CURLcode multi_done(struct Curl_easy *data,
     CONN_UNLOCK(data);
     if(Curl_conncache_return_conn(data, conn)) {
       /* remember the most recently used connection */
-      data->state.lastconnect = conn;
+      data->state.lastconnect_id = conn->connection_id;
       infof(data, "%s\n", buffer);
     }
     else
-      data->state.lastconnect = NULL;
+      data->state.lastconnect_id = -1;
   }
 
   Curl_free_request_state(data);
